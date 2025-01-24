3af99f051556ee124df33ecacd199ed66685df3a
Pedro <lumbipp@gmail.com>
2025-01-23 20:03:51 -0600

    Add model, Add controller empty

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+use App\Http\Controllers\api\studentController;
+
+/*
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+});
+*/
+
+Route::get('students', function () {
+    return "Get student List";
+});
+
+Route::get('students/{id}', function ($id) {
+    return "Get a student with ID: " . $id;
+});
+
+Route::post('students/{id}', function ($id) {
+    return "Create a student with ID: $id";
+});
+
+Route::put('students/{id}', function ($id) {
+    return "Updating student with ID: $id";
+});
+
+Route::delete('students/{id}', function ($id) {
+    return "Deleting student with ID: $id";
 });
